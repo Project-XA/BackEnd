52ca34430968b30e332ada7f57ac1890e7823663
Abdulrahman <abdoawwad043@gmail.com>
2025-12-08 20:44:51 +0200

    Add dockerfile

--- Program.cs
+++ Program.cs
@@ -103,11 +103,11 @@ namespace Project_X
             }
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseHttpsRedirection();
             app.UseCors("MyPolicy");
